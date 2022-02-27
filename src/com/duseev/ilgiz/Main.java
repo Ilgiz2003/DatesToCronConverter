062daac82273bbf58160bf569a9a979a2b23f0ce
Ilgiz <dussev.ig@edu.spbstu.ru>
2022-02-27 19:11:05 +0300

    Реализаци случая, когда найдены все поля удовлетворяющие >50 % дат, будет реализован максимально конкретный случай

--- src/com/duseev/ilgiz/Main.java
+++ src/com/duseev/ilgiz/Main.java
@@ -10,6 +10,7 @@ public class Main {
         realiz.print();
         realiz.fillingCronDates();
         realiz.printCronDates();
+        realiz.convert();
 
 
     }
--- src/com/duseev/ilgiz/Realization.java
+++ src/com/duseev/ilgiz/Realization.java
@@ -1,20 +1,21 @@
 package com.duseev.ilgiz;
 
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
 
 public class Realization {
     LinkedList<String> dates = new LinkedList<>();
     public void filling() {
-    String date1="2000-01-30T19:53:00";
-    String date2="2022-01-24T19:54:00";
-    String date3="2022-01-24T19:55:00";
-    String date4="2022-01-24T19:56:00";
-    String date5="2022-01-24T19:57:00";
-    String date6="2022-01-24T19:58:00";
-    String date7="2022-01-24T19:59:00";
-    String date8="2022-01-24T20:00:00";
-    String date9="2022-01-24T20:01:00";
-    String date10="2022-01-24T20:02:00";
+    String date1="2022-01-25T08:00:00";
+    String date2="2022-01-25T08:30:00";
+    String date3="2022-01-25T09:00:00";
+    String date4="2022-01-25T09:30:00";
+    String date5="2022-01-26T08:00:00";
+    String date6="2022-01-26T08:30:00";
+    String date7="2022-01-26T09:00:00";
+    String date8="2022-01-26T09:30:00";
         dates.add(date1);
         dates.add(date2);
         dates.add(date3);
@@ -23,8 +24,6 @@ public class Realization {
         dates.add(date6);
         dates.add(date7);
         dates.add(date8);
-        dates.add(date9);
-        dates.add(date10);
     }
     public void sort(){
         dates.sort(String::compareToIgnoreCase);
@@ -106,22 +105,122 @@ public class Realization {
         }
     }
     public void printCronDates() {
-        for (int i=0;i<10;i++) {
+        int ListSize=cronDates.size();
+        for (int i=0;i<ListSize;i++) {
             System.out.println(cronDates.get(i));
         }
     }
     public void convert(){
         int []countweekday=new int[7];
-        int []countmonth=new int[12];
-        int []countday=new int[31];
-        int []counthour=new int[23];
-        int []countminutes=new int[59];
-        int []countwseconds=new int[59];
+        int []countmonth=new int[13];
+        int []countday=new int[32];
+        int []counthour=new int[24];
+        int []countminutes =new int[60];
+        int []countseconds=new int[60];
+        String valuemaxSecondsCount="*";
+        String valuemaxMinutesCount="*";
+        String valuemaxHourCount="*";
+        String valuemaxDayCount="*";
+        String valuemaxMonthCount="*";
+        String valuemaxWeekdayCount="*";
+        int maxSecondsCount;
+        int maxMinutesCount;
+        int maxHourCount;
+        int maxDayCount;
+        int maxMonthCount;
+        int maxWeekdayCount;
+        int ListSize=cronDates.size();
+        for(int i=0;i<ListSize;i++){
+            countseconds[cronDates.get(i).seconds]++;
+        }
+        IntStream intStream = Arrays.stream(countseconds);
+        OptionalInt optionalInt = intStream.max();
+        maxSecondsCount = optionalInt.getAsInt();
+
+        if(maxSecondsCount > ListSize/2){
+            for(int i=0;i<59;i++){
+                if(countseconds[i]==maxSecondsCount){
+                    valuemaxSecondsCount = "" + i;
+                }
+            }
+            for(int i=0;i<ListSize;i++){
+                if(cronDates.get(i).seconds==Integer.parseInt(valuemaxSecondsCount.trim())){
+                    countminutes[cronDates.get(i).minutes]++;
+                }
+            }
+            intStream = Arrays.stream(countminutes);
+            optionalInt = intStream.max();
+            maxMinutesCount = optionalInt.getAsInt();
+            if(maxMinutesCount>ListSize/2){
+                for(int i=0;i<59;i++){
+                    if(countminutes[i]==maxMinutesCount){
+                        valuemaxMinutesCount = "" + i;
+                    }
+                }
+                //код для этого случая
+            }
+            else {
+                for(int i=0;i<ListSize;i++){
+                    if(cronDates.get(i).seconds==Integer.parseInt(valuemaxSecondsCount.trim())){
+                        counthour[cronDates.get(i).hour]++;
+                    }
+                }
+                intStream = Arrays.stream(counthour);
+                optionalInt = intStream.max();
+                maxHourCount = optionalInt.getAsInt();
+                if(maxHourCount > ListSize/2){
+                    for(int i=0;i<23;i++){
+                        if(counthour[i]==maxHourCount){
+                            valuemaxHourCount = "" + i;
+                        }
+                    }
+                    for(int i=0;i<ListSize;i++){
+                        if(cronDates.get(i).hour==Integer.parseInt(valuemaxHourCount.trim())){
+                            countday[cronDates.get(i).day]++;
+                        }
+                    }
+                    intStream = Arrays.stream(countday);
+                    optionalInt = intStream.max();
+                    maxDayCount = optionalInt.getAsInt();
+                    if(maxDayCount>ListSize/2){
+                        for(int i=0;i<32;i++){
+                            if(countday[i]==maxDayCount){
+                                valuemaxDayCount = "" + i;
+                            }
+                        }
+                        for(int i=0;i<ListSize;i++){
+                            if(cronDates.get(i).day==Integer.parseInt(valuemaxDayCount.trim())){
+                                countmonth[cronDates.get(i).month]++;
+                            }
+                        }
+                        intStream = Arrays.stream(countmonth);
+                        optionalInt = intStream.max();
+                        maxMonthCount = optionalInt.getAsInt();
+                        if(maxMonthCount>ListSize/2){
+                            for(int i=0;i<13;i++){
+                                if(countmonth[i]==maxMonthCount){
+                                    valuemaxMonthCount = "" + i;
+                                }
+                            }
+                        }
+                    }
+                    else{
+                        valuemaxDayCount = "*";
+                        //код для этого случая
+
+                    }
+                }
+                else {
+                    valuemaxHourCount = "*";
+                    //код для этого случая
+
+                }
+            }
+        }
 
 
-
-
-
+        String result=valuemaxSecondsCount + " " + valuemaxMinutesCount + " " + valuemaxHourCount+ " " +valuemaxDayCount+ " " +valuemaxMonthCount+ " " +valuemaxWeekdayCount;
+        System.out.println(result);
     }
     public String getImplementationInfo;
 }
